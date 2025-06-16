b5b521fcb8c2f8c8c99a096c96054f2336de8ad9
nagacharankumarreddy <nagacharankumarreddy@gmail.com>
2025-06-17 00:32:19 +0530

    base path added in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 import { componentTagger } from "lovable-tagger";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: "/triplyplan/",
   plugins: [
     react(),
     mode === 'development' &&
